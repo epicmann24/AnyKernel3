58fff870c2b6178cf3f72f195d3d6c1228ab93d1
epicmann24 <110631325+epicmann24@users.noreply.github.com>
2025-06-04 15:25:01 +0100

    Configure anykernel logic

--- anykernel.sh
+++ anykernel.sh
@@ -4,16 +4,16 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
-do.devicecheck=1
+kernel.string=Epicmann24s kernel by epicmann24 @ xda-developers epkmn @ telegram
+do.devicecheck=0
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=
+device.name2=
+device.name3=
+device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=
@@ -22,100 +22,214 @@ supported.vendorpatchlevels=
 
 
 ### AnyKernel install
-## boot files attributes
-boot_attributes() {
-set_perm_recursive 0 0 755 644 $RAMDISK/*;
-set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-} # end attributes
-
-# boot shell variables
-BLOCK=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-IS_SLOT_DEVICE=0;
-RAMDISK_COMPRESSION=auto;
-PATCH_VBMETA_FLAG=auto;
+## boot shell variables
+block=boot
+is_slot_device=auto
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
+no_magisk_check=1
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh;
+. tools/ak3-core.sh
+
+kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
+case $kernel_version in
+    5.1*) ksu_supported=true ;;
+    6.1*) ksu_supported=true ;;
+    6.6*) ksu_supported=true ;;
+    *) ksu_supported=false ;;
+esac
+
+ui_print " " "  -> ksu_supported: $ksu_supported"
+$ksu_supported || abort "  -> Non-GKI device, abort."
 
 # boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
+    split_boot # for devices with init_boot ramdisk
+    flash_boot # for devices with init_boot ramdisk
+else
+    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+fi
 ## end boot install
 
 
-## init_boot files attributes
-#init_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $RAMDISK/*;
-#set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-#} # end attributes
+rm -f /data/adb/service.d/kernel-conf.sh
+mkdir -p /data/adb/service.d
+touch /data/adb/service.d/kernel-conf.sh
+rm -f /data/adb/post-fs-data.d/kernel-conf.sh
+mkdir -p /data/adb/post-fs-data.d
+touch /data/adb/post-fs-data.d/kernel-conf.sh
 
-# init_boot shell variables
-#BLOCK=init_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
+chmod +x /data/adb/post-fs-data.d/kernel-conf.sh
+chmod +x /data/adb/service.d/kernel-conf.sh
 
-# reset for init_boot patching
-#reset_ak;
+# ===START DTBO PATCH===
 
-# init_boot install
-#dump_boot; # unpack ramdisk since it is the new first stage init ramdisk where overlay.d must go
+#Credits to bybycode
 
-#write_boot;
-## end init_boot install
+MODPATH=$(dirname "$(readlink -f "$0")")
 
+lfdtget=$MODPATH/bin/fdtget
+lfdtput=$MODPATH/bin/fdtput
 
-## vendor_kernel_boot shell variables
-#BLOCK=vendor_kernel_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
+PATCH_DTB() {
+    ui_print "Patching $1"
+    local lfinded=0
+    local alpatched=0
 
-# reset for vendor_kernel_boot patching
-#reset_ak;
+    for i in $($lfdtget $1 /__fixups__ soc); do
+        local lpath=$(echo $i | sed 's/\:target\:0//g')
+        if $lfdtget -l $1 ${lpath}/__overlay__ | grep -q hmbird; then
+            if [ $($lfdtget $1 ${lpath}/__overlay__/oplus,hmbird/version_type type) == "HMBIRD_GKI" ]; then
+                alpatched=1
+                ui_print "$1 has been patched"
+            fi
+            break
+        fi
+    done
 
-# vendor_kernel_boot install
-#split_boot; # skip unpack/repack ramdisk, e.g. for dtb on devices with hdr v4 and vendor_kernel_boot
+    if [ $alpatched -eq 0 ]; then
+        for i in $($lfdtget $1 /__fixups__ soc); do
+            local lpath=$(echo $i | sed 's/\:target\:0//g')
+            if $lfdtget -l $1 ${lpath}/__overlay__ | grep -q hmbird; then
+                local lfinded=1
+                ui_print "- $1 Found DTBO patch location"
+                $lfdtput -t s $1 ${lpath}/__overlay__/oplus,hmbird/version_type type HMBIRD_GKI
+                break
+            fi
+        done
 
-#flash_boot;
-## end vendor_kernel_boot install
+        if [ $lfinded -eq 0 ]; then
+            ui_print "- Add patches for non GKI: $1"
+            for i in $($lfdtget $1 /__fixups__ soc); do
+                local ppath=$(echo $i | sed 's/\:target\:0//g')
+                if $lfdtget -l $1 ${ppath}/__overlay__ | grep -q reboot_reason; then
+                    $lfdtput -p -c $1 ${ppath}/__overlay__/oplus,hmbird/version_type
+                    $lfdtput -t s $1 ${ppath}/__overlay__/oplus,hmbird/version_type type HMBIRD_GKI
+                    break
+                fi
+            done
+        fi
+    fi
+}
 
+REPACKDTBO() {
+    ui_print ""
+    ui_print ""
+    ui_print "Start DTBO patch"
+    ui_print ""
+    ui_print ""
+    LMKDT=$MODPATH/bin/mkdtimg
+    ui_print "Unpacking DTBO"
+    $LMKDT dump $DTBOTMP -b dtb >/dev/null 2>&1
+    wait
 
-## vendor_boot files attributes
-#vendor_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $RAMDISK/*;
-#set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-#} # end attributes
+    for i in dtb.*; do
+        PATCH_DTB $i &
+    done
+    wait
+    ui_print "Packaging DTBO"
 
-# vendor_boot shell variables
-#BLOCK=vendor_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
+    $LMKDT create $DTBOTMP --page_size=4096 dtb.* >/dev/null 2>&1
+    wait
+}
 
-# reset for vendor_boot patching
-#reset_ak;
+model=$(getprop ro.product.vendor.name)
+ui_print "Model code: $model"
+ui_print ""
 
-# vendor_boot install
-#dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
+DTBO_PARTI="/dev/block/bootdevice/by-name/dtbo$(getprop ro.boot.slot_suffix)"
+DTBOTMP="${TMPDIR}/dtbo.img"
 
-#write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-## end vendor_boot install
+chmod +x $MODPATH/bin/*
+dd if=$DTBO_PARTI of=$DTBOTMP
+REPACKDTBO
+ui_print ""
+ui_print ""
+ui_print "Flashing DTBO"
+dd if=$DTBOTMP of=$DTBO_PARTI
+
+rm -r $MODPATH/bin
+rm -r $MODPATH/patch
+
+# ===END DTBO PATCH===
+
+cat <<'sd'>> /data/adb/service.d/kernel-conf.sh
+#!/system/bin/sh
+while [ "$(getprop sys.boot_completed)" != "1" ]; do
+sleep 5
+done
+sleep 5
+echo "off" > /proc/sys/kernel/printk_devkmsg
+for disks in /sys/block/*/queue; do
+echo 0 > "$disks/iostats"
+done
+echo 0 > /proc/sys/kernel/sched_schedstats
+echo "4674" > /sys/kernel/oplus_display/max_brightness
+echo "0" > /sys/devices/system/cpu/pmu_lib/enable_counters
+echo "1888" > /sys/class/qcom-haptics/vmax
+echo "8300" > /sys/class/qcom-haptics/cl_vmax
+echo "11451" > /sys/class/qcom-haptics/fifo_vmax
+stop statsd
+stop tombstoned
+stop criticallog
+stop traced
+stop traced_probes
+sd
+
+cat <<'pfsd'>> /data/adb/post-fs-data.d/kernel-conf.sh
+#!/system/bin/sh
+if ! echo $(uname -r) | grep -q "Epicmann24"; then
+rm -f /data/adb/service.d/kernel-conf.sh
+rm -f /data/adb/post-fs-data.d/kernel-conf.sh
+exit 0
+fi
+echo fq > /proc/sys/net/core/default_qdisc 2>/dev/null
+echo 1 > /proc/sys/net/ipv4/tcp_window_scaling 2>/dev/null
+echo "4096 87380 16777216" > /proc/sys/net/ipv4/tcp_rmem 2>/dev/null
+echo "4096 65536 16777216" > /proc/sys/net/ipv4/tcp_wmem 2>/dev/null
+echo 16777216 > /proc/sys/net/core/rmem_max 2>/dev/null
+echo 16777216 > /proc/sys/net/core/wmem_max 2>/dev/null
+echo 4096 > /proc/sys/net/ipv4/tcp_max_syn_backlog 2>/dev/null
+echo 1 > /proc/sys/net/ipv4/tcp_mtu_probing 2>/dev/null
+echo 1 > /proc/sys/net/ipv6/tcp_ecn 2>/dev/null
+echo "4096 87380 16777216" > /proc/sys/net/ipv6/tcp_rmem 2>/dev/null
+echo "4096 65536 16777216" > /proc/sys/net/ipv6/tcp_wmem 2>/dev/null
+resetprop persist.logd.flowctrl.on 0
+resetprop persist.logd.flowctrl.method 0
+resetprop ro.logd.flowctrl.on 0
+resetprop ro.logd.flowctrl.method 0
+resetprop debug.oplus.video.log.enable 0
+resetprop persist.sys.log.user 0
+resetprop persist.sys.oplus.bt.cache_hcilog_mode 0
+resetprop persist.sys.oplus.need_log 0
+resetprop vendor.swvdec.log.level 0
+resetprop persist.sys.ostats_tpd.enable 0
+resetprop persist.sys.ostats_pullerd.enable 0
+resetprop persist.sys.ostatsd.enable 0
+resetprop debug.sf.oplus_display_trace.enable 0
+resetprop net.core.default_qdisc fq
+resetprop net.ipv4.tcp_congestion_control bbr
+mount --bind /data/local/tmp/empty /system_ext/app/CrashBox
+mount --bind /data/local/tmp/empty /system_ext/app/EidService
+mount --bind /data/local/tmp/empty /system_ext/app/LogKit
+mount --bind /data/local/tmp/empty /system_ext/app/Olc
+mount --bind /data/local/tmp/empty /system_ext/app/OplusLocationService
+mount --bind /data/local/tmp/empty /system_ext/app/OTrace
+mount --bind /data/local/tmp/empty /system_ext/app/QCC
+mount --bind /data/local/tmp/empty /system_ext/app/LFEHer
+mount --bind /data/local/tmp/empty /system_ext/app/OplusQualityProtect
+mount --bind /data/local/tmp/empty /system_ext/priv-app/com.qualcomm.location
+mount --bind /data/local/tmp/empty /product/app/StdSP
+mount --bind /data/local/tmp/empty /product/app/LocationProxy
+mount --bind /data/local/tmp/empty /product/priv-app/DCS
+mount --bind /data/local/tmp/empty /product/priv-app/Metis
+mount --bind /data/local/tmp/empty /vendor/app/TxPwrAdmin
+mount --bind /data/local/tmp/empty /vendor/app/TrustZoneAccessService
+mount --bind /data/local/tmp/empty /system_ext/priv-app/xrvdservice
+mount --bind /data/local/tmp/empty /product/app/DeviceStatisticsService
+mount --bind /data/local/tmp/empty /system_ext/app/OwkService
+mount --bind /data/local/tmp/empty /my_stock/non_overlay/app/OBrain
+pfsd
 
