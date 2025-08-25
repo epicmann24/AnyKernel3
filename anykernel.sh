cceac329704b440f21bb37678d9ced72bd239492
epicmann24 <110631325+epicmann24@users.noreply.github.com>
2025-08-25 12:19:00 +0100

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -505,31 +505,7 @@ resetprop ro.boot.veritymode enforcing
 
 mkdir -p /data/local/tmp/empty
 
-mount --bind /data/local/tmp/empty /system_ext/app/CrashBox
-mount --bind /data/local/tmp/empty /system_ext/app/EidService
-mount --bind /data/local/tmp/empty /system_ext/app/LogKit
-mount --bind /data/local/tmp/empty /system_ext/app/Olc
-mount --bind /data/local/tmp/empty /system_ext/app/OplusLocationService
-mount --bind /data/local/tmp/empty /system_ext/app/OTrace
-mount --bind /data/local/tmp/empty /system_ext/app/QCC
-mount --bind /data/local/tmp/empty /system_ext/app/LFEHer
-mount --bind /data/local/tmp/empty /system_ext/app/OplusQualityProtect
-mount --bind /data/local/tmp/empty /system_ext/priv-app/com.qualcomm.location
-mount --bind /data/local/tmp/empty /system_ext/app/QesdkSysService
-mount --bind /data/local/tmp/empty /product/app/StdSP
-mount --bind /data/local/tmp/empty /product/priv-app/DCS
-mount --bind /data/local/tmp/empty /product/priv-app/Metis
-mount --bind /data/local/tmp/empty /vendor/app/TxPwrAdmin
-mount --bind /data/local/tmp/empty /vendor/app/TrustZoneAccessService
-mount --bind /data/local/tmp/empty /system_ext/priv-app/xrvdservice
-mount --bind /data/local/tmp/empty /product/app/DeviceStatisticsService
-mount --bind /data/local/tmp/empty /system_ext/app/OwkService
-mount --bind /data/local/tmp/empty /my_stock/non_overlay/app/OBrain
-mount --bind /data/local/tmp/empty /system_ext/app/SoterService
-mount --bind /data/local/tmp/empty /system_ext/app/TTSatelliteAuth
-mount --bind /data/local/tmp/empty /product/app/MCS
 mount --bind /data/local/tmp/empty /product/priv-app/Facebook-installer
 mount --bind /data/local/tmp/empty /product/app/Facebook-appmanager
-mount --bind /data/local/tmp/empty /system/app/Traceur
 
 psfd
